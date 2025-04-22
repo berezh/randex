b90a1e0d23e4fdf3762f1e7be188cca127883fdf
Berezh <verezh@gmail.com>
2025-04-23 01:52:01 +0300

    v

--- src/custom/index.ts
+++ src/custom/index.ts
@@ -60,37 +60,6 @@ export class Randex {
 
   public static singleName = randexSingleName;
 
-  public static numberArray(length: RandexRange, count: number): number[] {
-    let min = 0;
-    let max = 0;
-    const result: number[] = [];
-
-    if (typeof length === "number") {
-      max = length;
-    } else {
-      min = length[0];
-      max = length[1];
-    }
-
-    const maxCount = max - min + 1;
-
-    const numberArray: number[] = [];
-    for (let i = min; i <= max; i++) {
-      numberArray.push(i);
-    }
-
-    for (let i = 0; i < count && i < maxCount; i++) {
-      const arrayCount = numberArray.length - 1;
-      const index = Randex.number(arrayCount);
-
-      const value = numberArray[index];
-      result.push(value);
-      numberArray.splice(index, 1);
-    }
-
-    return result;
-  }
-
   public static number = randexNumber;
 
   public static username = randexUsername;
